26e6664c8f37e2a60cca9e26998fffc7e7e45d78
Sócrates Freitas <68630680+Socrates-Freitas@users.noreply.github.com>
2024-01-30 23:00:25 -0300

    Correção da horientação das imagens da pagina

--- src/Components/PageContainer/LoginFormECadastro/ImagesContainer.tsx
+++ src/Components/PageContainer/LoginFormECadastro/ImagesContainer.tsx
@@ -5,9 +5,16 @@ import styled from "styled-components";
 const FormImageContainer = styled.div`
     width: 100%;
     display: flex;
-    flex-direction: column;
-    align-items: center;
-    row-gap: 2vh;
+    flex-direction: row;
+    justify-content: center;
+    column-gap: 2vw;
+
+    @media screen and (max-width:800px) {
+        flex-direction: column;
+        align-items: center;
+        column-gap: 0vw;
+        row-gap: 2vh;
+    }
 `
 
 export default FormImageContainer
